6c7cbcf210e88cd331d873f763cff43296bb930e
yangkun <yangkun@icourt.cc>
2023-04-19 18:52:29 +0800

    style: 样式文件修改

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,3 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World!
-    </main>
-  );
+  return <div>123 11 324</div>;
 }
